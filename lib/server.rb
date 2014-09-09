a237adf7d3f35fe4f5461146197bd6a2bc73f893
byverdu <byverdu@gmail.com>
2014-09-09 06:53:23 +0100

    test class tag done

--- lib/server.rb
+++ lib/server.rb
@@ -11,6 +11,7 @@ class Bookmark < Sinatra::Base
 	DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 
 	require './lib/link'
+	require './lib/tag'
 
 	DataMapper.finalize
 
@@ -29,9 +30,24 @@ class Bookmark < Sinatra::Base
 		url   = params["url"]
 		title = params["title"]
 
-		Link.create(:url => url, :title => title)
+		tags = params["tags"].split(" ").map do |tag|
+
+			Tag.first_or_create(:text => tag)
+
+		end
+
+		Link.create(:url => url, :title => title, :tags => tags)
 
 		redirect to('/')
+	end
+
+
+	get '/tags/:text' do
+
+		tag    = Tag.first(:text => params[:text])
+		@links = tag ? tag.links : []
+
+		erb :index
 	end
 
 end
