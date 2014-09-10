3020d7124972a85b5d3fa853c09cd867add215ca
byverdu <byverdu@gmail.com>
2014-09-10 11:38:19 +0100

    password checking error same email

--- app/server.rb
+++ app/server.rb
@@ -57,14 +57,14 @@ class Bookmark < Sinatra::Base
 
 	post '/users' do
 		@user = User.create(:email          => params[:email],
-								:password              => params[:password],
-								:password_confirmation => params[:password_confirmation])
+												:password              => params[:password],
+												:password_confirmation => params[:password_confirmation])
 
 		if @user.save
 			session[:user_id] = @user.id
 			redirect to('/')
 		else
-			flash[:notice] = "Sorry, your passwords don't match"
+			flash.now[:errors] = @user.errors.full_messages
 			erb :"users/new"
 		end
 	end
