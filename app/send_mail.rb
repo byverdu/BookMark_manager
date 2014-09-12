4f5330d8e99ae03c041665c610a8ef6988debe3c
byverdu <byverdu@gmail.com>
2014-09-12 01:00:28 +0100

    added token module

--- app/send_mail.rb
+++ app/send_mail.rb
@@ -1,9 +1,11 @@
-require 'mailgun'
+require 'rest-client'
+require_relative 'token'
 
 module SendMail
     
     def email_confirmation(email,token)
-      RestClient.post "https://api:key-532b956c162b4c69d03c7000d2c66d74@api.mailgun.net/v2/sandbox1dc7eee330cc42f2948b94e7cf7284a2.mailgun.org/messages",
+      RestClient.post "https://api:#{Token::API_KEY}"\
+      "@api.mailgun.net/v2/sandbox1dc7eee330cc42f2948b94e7cf7284a2.mailgun.org/messages",
       :from    => "Support@byverdu-bookmark.com",
       :to      => "#{email}",
       :subject => "Recovering email password",
