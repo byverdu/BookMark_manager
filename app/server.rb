aa11d3ffb87e96495edb3d270a15713a0aa20519
byverdu <byverdu@gmail.com>
2014-09-12 19:59:12 +0100

    added some comments on server.rb

--- app/server.rb
+++ app/server.rb
@@ -1,11 +1,17 @@
+=begin
+
+Server file, all gems and files needed required in this location
+
+=end
+
 require "sinatra/base"
 require "data_mapper"
 require "rack-flash"
 require 'sinatra/partial'
 
-require_relative 'data_mapper_setup'
-require_relative 'helpers/application'
-require_relative 'send_mail'
+require_relative 'data_mapper_setup'   # App models inside this file 
+require_relative 'helpers/application' # current_user helper
+require_relative 'send_mail'  				 # mailgun 
 
 require_relative './controllers/application'
 require_relative './controllers/links'
@@ -15,7 +21,6 @@ require_relative './controllers/users'
 
 
 class Bookmark < Sinatra::Base
-
 
 	register Sinatra::Partial
 
@@ -30,7 +35,6 @@ class Bookmark < Sinatra::Base
 
 	use Rack::Flash
 	use Rack::MethodOverride
-
 end
 
 
