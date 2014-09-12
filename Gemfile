28888a3a38e198a1340f33df1dd80aef7198dba1
byverdu <byverdu@gmail.com>
2014-09-12 11:15:48 +0100

    partials views added

--- Gemfile
+++ Gemfile
@@ -4,16 +4,17 @@ ruby '2.1.2'
 
 gem 'data_mapper'
 gem 'dm-postgres-adapter'
+gem 'mailgun'
 gem 'rack-flash3'
 gem 'sinatra'
-gem 'mailgun'
+gem 'sinatra-partial'
 
 
 group :development, :test do
 
 	gem 'capybara'
 	gem 'database_cleaner'
-	gem 'rspec'
 	gem 'launchy'
+	gem 'rspec'
 
 end
--- app/server.rb
+++ app/server.rb
@@ -1,6 +1,7 @@
 require "sinatra/base"
 require "data_mapper"
 require "rack-flash"
+require 'sinatra/partial'
 
 require_relative 'data_mapper_setup'
 require_relative 'helpers/application'
@@ -9,11 +10,14 @@ require_relative 'send_mail'
 
 class Bookmark < Sinatra::Base
 
+	register Sinatra::Partial
+
 	include ApplicationHelpers
   include SendMail
 
 	set :views, File.join(root,'views')
 	set :session_secret, 'super secret'
+	set :partial_template_engine, :erb
 	
 	enable :sessions
 
